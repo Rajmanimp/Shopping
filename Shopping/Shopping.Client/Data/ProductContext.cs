ac231f61aed8015fd79fa231518c9dfff908293d
Rajmani Patel <rajmanipatel@microsoft.com>
2023-12-20 10:52:11 +0530

    added first phase

---
+++ Shopping/Shopping.Client/Data/ProductContext.cs
@@ -0,0 +1,27 @@
+﻿using Shopping.Client.Models;
+
+namespace Shopping.Client.Data
+{
+    public static class ProductContext
+    {
+        public static readonly List<Product> products = new List<Product>()
+        {
+            new Product()
+            {
+                Name = "Iphone X",
+                Description = "Phone Description",
+                ImageFile = "product1.png",
+                Price = 100.00M,
+                Category = "Smart Phone"
+            },
+            new Product()
+            {
+                Name = "Samsung Galaxy S21",
+                Description = "Phone Description",
+                ImageFile = "product1.png",
+                Price = 150.00M,
+                Category = "Smart Phone"
+            }
+        };
+    }
+}
